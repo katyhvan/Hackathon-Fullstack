5f572ade8ede52ea5a87496e694b8924692fcc80
Katerina <katyhvan@gmail.com>
2022-11-01 23:44:06 +0600

    added menudropdown, responsive layout of navbar

--- src/components/Navbar/MenuDropdown.jsx
+++ src/components/Navbar/MenuDropdown.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const MenuDropdown = () => {
+import "../../styles/MenuDropDown.css";
+
+const pages = [
+  {
+    type: "Home",
+    path: "/",
+  },
+  {
+    type: "Courses",
+    path: "/courses",
+  },
+  {
+    type: "Favorites",
+    path: "/favorites",
+  },
+  {
+    type: "Shop",
+    path: "/cart",
+  },
+];
+
+const MenuDropdown = ({ active, setActive }) => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <div>MenuDropdown</div>
-    </>
+    <div
+      className={active ? "menu-dropdown active" : "menu-dropdown"}
+      onCLick={() => setActive(false)}
+    >
+      {pages.map((page) => (
+        <div key={page.type} onClick={() => navigate(page.path)}>
+          {page.type}
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo1.png";
 import { useNavigate } from "react-router-dom";
 import NavItemDropDown from "./NavItemDropDown";
+import MenuDropdown from "./MenuDropdown";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import Avatar from "@mui/material/Avatar";
+import { Logout } from "@mui/icons-material";
 
 import "../../styles/Navbar.css";
 
-const pages = [
-  {
-    type: <ShoppingBagIcon />,
-    path: "/cart",
-  },
-  {
-    type: <Avatar />,
-    path: "/login",
-  },
-  {
-    type: "Sign Up",
-    path: "/register",
-  },
-];
-
 const Navbar = () => {
   const navigate = useNavigate();
+  const [menuActive, setMenuActive] = useState();
+
   return (
-    <div className="header">
-      <div className="navbar-link">
-        <img src={logo} alt="logo" className="logo" />
-        <h2 className="logo-title">Medemy</h2>
-      </div>
-      <nav>
-        <ul className="navbar-menu">
-          <li className="navbar-item" onClick={() => navigate("/")}>
-            Home
-          </li>
-          <li className="navbar-item">
-            <NavItemDropDown />
-          </li>
-          <li className="navbar-item" onClick={() => navigate("/favorites")}>
-            Favorites
-          </li>
-        </ul>
-      </nav>
-      <div className="navbar-right">
-        <ul className="right-menu">
-          {pages.map((page) => (
-            <li
-              key={page.type}
-              onClick={() => navigate(page.path)}
-              className="right-item"
-            >
-              {page.type}
+    <>
+      <div className="header">
+        <div className="navbar-link">
+          <img src={logo} alt="logo" className="logo" />
+          <h2 className="logo-title">Medemy</h2>
+        </div>
+        <div className="burger-menu" onClick={() => setMenuActive(!menuActive)}>
+          <div className="burger-btn"></div>
+          <div className="burger-btn"></div>
+          <div className="burger-btn"></div>
+        </div>
+        <nav>
+          <ul className="navbar-menu">
+            <li className="navbar-item" onClick={() => navigate("/")}>
+              Home
             </li>
-          ))}
-          <li className="right-item">Logout</li>
-        </ul>
+            <li className="navbar-item">
+              <NavItemDropDown />
+            </li>
+            <li className="navbar-item" onClick={() => navigate("/favorites")}>
+              Favorites
+            </li>
+          </ul>
+        </nav>
+        <div className="navbar-right">
+          <ul className="right-menu">
+            <ShoppingBagIcon
+              className="right-item"
+              onClick={() => navigate("/cart")}
+            />
+            <Avatar
+              className="right-item avatar"
+              onClick={() => navigate("/login")}
+            />
+            <li
+              className="right-item register"
+              onClick={() => navigate("/register")}
+            >
+              Sign Up
+            </li>
+            <li className="right-item logout">Logout</li>
+            <Logout className="logout-icon" />
+          </ul>
+        </div>
       </div>
-    </div>
+      <MenuDropdown active={menuActive} setActive={setMenuActive} />
+    </>
   );
 };
 
