65cc7255a7e90420e97265a64f4e74b847e6405e
Пользователь <ziyadaturdukulova@gmail.com>
2022-11-04 12:52:29 +0300

    added

--- src/components/courses/AddCourse.jsx
+++ src/components/courses/AddCourse.jsx
@@ -2,17 +2,18 @@ import { Category } from "@mui/icons-material";
 import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { coursesContext } from "../../contesxts/CoursesContextProvider";
+import "../../styles/AddCourse.css"
 
 const AddCourse = () => {
   const navigate = useNavigate();
 
-  const { courses, addCourse } = useContext(coursesContext);
+  const { courses, addCourse, categories } = useContext(coursesContext);
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("");
-  const [video, setVideo] = useState(null);
+  const [title, setTitle] = useState(""); 
+  const [description, setDescription] = useState(""); 
+  const [price, setPrice] = useState(""); 
+  const [category, setCategory] = useState(""); 
+  const [image, setImage] = useState(null);
 
   // useEffect(() => {
   //   getCategories();
@@ -20,42 +21,53 @@ const AddCourse = () => {
 
   function handleAdd() {
     let newCourse = new FormData();
-    newCourse.append("video", video);
     newCourse.append("title", title);
     newCourse.append("description", description);
     newCourse.append("price", price);
     newCourse.append("category", category);
+    newCourse.append("image", image);
     addCourse(newCourse, navigate);
   }
 
   return (
     <>
-      <div>
-        <h2>AddCourse</h2>
+      <div className="add-block">
+        <h2 className="add-title">Your Course</h2>
         <input
+          className="add-inp"
           type="text"
           value={title}
           placeholder="Title"
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
+          className="add-inp"
           type="text"
           value={description}
           placeholder="Description"
           onChange={(e) => setDescription(e.target.value)}
         />
         <input
+          className="add-inp"
           type="text"
           placeholder="Price"
           onChange={(e) => setPrice(e.target.value)}
         />
+        <select className="chooseCategory" value={category} onChange={(e) => setCategory(e.target.value)}>
+			   <option>Choose category...</option>
+			   {categories?.map(item => (
+				 <option key={item.id} value={item.id}>{item.title}</option>
+			   ))}
+			  </select>
 
         <input
-          type="text"
-          placeholder="Video"
-          onChange={(e) => setVideo(e.target.files)}
+          type="file"  
+          accept="image"
+          className="add-inp"
+          placeholder="Image"
+          onChange={(e) => setImage(e.target.files[0])}
         />
-        <button onClick={handleAdd}>Add Course</button>
+        <button className="add-btn" onClick={handleAdd}>Add</button>
       </div>
     </>
   );
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import error from ".././assets/error.jpg"
 
 const NotFoundPage = () => {
   return (
-    <>
-      <div>NotFoundPage</div>
-    </>
+      <div style={{display: 'flex', justifyContent: "center", marginTop: "3%"}}>
+      <img style={{width: '60%', height: '60%'}} src={error} alt="error" />
+      </div>
   );
 };
 
