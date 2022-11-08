6c2715be0c8dae87daf0c1273e7f75a0aed1cacb
Katerina <katyhvan@gmail.com>
2022-11-08 23:51:29 +0600

    added changes for deploy

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,16 +2,14 @@ import React, { useContext, useEffect } from "react";
 import { shopContext } from "../../contesxts/ShopContextProvider";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import { styled } from "@mui/material/styles";
-import { tableCellClasses } from "@mui/material/TableCell";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Typography } from "@mui/joy";
-import { Button, TextField } from "@mui/material";
+
 import DeleteSweepIcon from "@mui/icons-material/DeleteSweep";
 
 import "../../styles/Shop.css";
+import { TextField } from "@mui/material";
 
 // const StyledTableCell = styled(TableCell)(({ theme }) => ({
 //   [`&.${tableCellClasses.head}`]: {
